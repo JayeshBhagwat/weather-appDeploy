0f6d278bbf886183f52050009474a41e22db0139
JayeshBhagwat <jayeshbhagwat7271@gmail.com>
2022-04-11 09:46:21 +0530

    worked on responsive-ness

--- src/app.js
+++ src/app.js
@@ -1,23 +1,19 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -30,7 +26,7 @@ app.get('', (req, res) => {
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
         return res.send({
-            error: 'You must provide an address!'
+            error: 'You must provide an Location!'
         })
     }
 
